6b91e9caea39f66f142f7ff0d2a5086e02570577
DarshitDev11 <darshitramani47@gmail.com>
2025-07-11 18:19:04 +0530

    changes..

--- server/index.js
+++ server/index.js
@@ -17,19 +17,12 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.DB_URI;
 mongoose.connect(MONGO_URI).then(()=>{console.log('MongoDB connected')}).catch((err)=>{console.log(err)});
 
-const allowedOrigins = process.env.ORIGIN.split(',');
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+  origin: [process.env.ORIGIN],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+  credentials: true,
+}))
+
 
 app.use(cookieParser());
 app.use(express.json());
